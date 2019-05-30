b75ee23481c169691e021d25e20ab991e121e66e
Tornike Gagadze <t.gagadze@owl.local>
2019-05-30 18:44:06 +0400

    Removed App.css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Submit from "../UI/button/Button";
+import Input from "../UI/Input/Input";
 class Login extends Component {
   state = {};
   render() {
     return (
       <div>
         <form>
-          <input />
-          <input />
+          <Input errors={{}} />
+          <Input errors={{}} />
           <Submit value="Login" />
         </form>
       </div>
