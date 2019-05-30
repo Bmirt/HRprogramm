0cc73e79f4f04377fcfa673b80c6887e36799d01
Tornike Gagadze <t.gagadze@owl.local>
2019-05-30 17:21:27 +0400

    created project structure

---
+++ src/components/Login/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class Login extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <form>
+          <input />
+          <input />
+          <button>login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
