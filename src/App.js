8232efa92578e84582119640dbfb15b97d95b4fc
Guram Chechelashvili <g.chechelashvili@owl.local>
2019-05-30 18:19:14 +0400

    inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Login from "./components/Login/Login";
+import Input from './components/UI/Input/Input'
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Route path="/Login" component={Login} />
+        <Input placeholder="Username" type="text" errors={{username:"username invalid", password:"password invalid"}} />
         {/* <Route path="/ResetPassword" component={Login} /> */}
       </Router>
     </div>
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Classes from './Input.module.css'
+export class Input extends Component {
+    render() {
+        return (
+            <div>
+                <input 
+                placeholder={this.props.placeholder}
+                type={this.props.type}
+                name="username"
+                className={Classes.input}/>
+                <div>
+                {this.props.errors.username && <span className={Classes.input_error}>{this.props.errors.username} </span>}
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Input
