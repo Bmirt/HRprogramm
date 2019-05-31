dde64ab06d2d4af5d8bf8a1709468c87a94758a9
Tornike Gagadze <t.gagadze@owl.local>
2019-05-31 10:35:38 +0400

    Placed reset link into login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import styles from './Login.module.css';
+import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 import Submit from "../UI/button/Button";
 import Input from "../UI/Input/Input";
@@ -12,8 +12,8 @@ class Login extends Component {
           <Input type="text" errors={{}} placeholder="Email or Username" />
           <Input type="password" errors={{}} placeholder="Password" />
           <Submit value="Login" />
+          <Link to="/reset-password">Forgot Password?</Link>
         </form>
-        <Link to="/reset-password">Forgot Password?</Link>
       </div>
     );
   }
