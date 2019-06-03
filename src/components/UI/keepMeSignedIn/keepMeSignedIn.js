6fc74b6851a721733670121cc3dc3f306230f529
Tornike Gagadze <tgagadze@gmail.com>
2019-06-03 11:42:13 +0400

    checkbox

--- src/components/UI/keepMeSignedIn/keepMeSignedIn.js
+++ src/components/UI/keepMeSignedIn/keepMeSignedIn.js
@@ -14,21 +14,20 @@ export default class keepMeSignedIn extends Component {
   render() {
     return (
       <div className={styles.container}>
-        <span>
-          <label className={styles.checkBoxContainer}>
-            <input
-              onClick={this.changeCheckBoxState}
-              className={styles.containerInput}
-              type="checkbox"
-              checked={this.state.isCheked}
-            />
-            <span className={styles.checkmark} />
-          </label>
-          Keep me signed in
-        </span>
-        <Link to="/reset-password">
-          <span>Forgot Password</span>
-        </Link>
+        <div className={styles.checkBoxContainer}>
+          <input
+            onClick={this.changeCheckBoxState}
+            className={styles.containerInput}
+            type="checkbox"
+            checked={this.state.isCheked}
+          />
+          <span>Keep me Logged In</span>
+        </div>
+        <div>
+          <Link to="/reset-password">
+            <span className={styles.link}>Forgot Password?</span>{" "}
+          </Link>
+        </div>
       </div>
     );
   }
