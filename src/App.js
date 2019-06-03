be5e1250c70a5741991ed968d350251a58d67719
Tornike Gagadze <tgagadze@gmail.com>
2019-06-03 14:16:11 +0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Login from "./components/Login/Login";
-import Input from "./components/UI/Input/Input";
 import "./App.css";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ResetPassword from "./components/ResetPassword/ResetPassword";
-import Header from "./components/Header/Header";
 
 function App() {
   return (
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import axios from "axios";
 import Form from "../Form/Form";
 import Submit from "../UI/button/Button";
 import Input from "../UI/Input/Input";
