6096ecca94646f2ea06a910ec59cdd1115a9fa23
Tamar Menteshashvili <t.menteshashvili@owl.local>
2019-06-07 12:47:32 +0400

    sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Sidebar.module.css";
+import menuIcon from '../../menuIcon.png'
+import profilesIcon from '../../profilesIcon.png'
+import projectsIcon from '../../projectsIcon.png'
+import analyticsIcon from '../../analyticsIcon.png'
+import calendarIcon from '../../calendarIcon.png'
+import blackListIcon from '../../blackListIcon.png'
+import userManagmentIcon from '../../userManagmentIcon.png'
 
 export class Sidebar extends Component {
   state = {
-    open: true
+    open: false
   };
   handleClick = e => {
     if (this.state.open) {
@@ -17,33 +24,46 @@ export class Sidebar extends Component {
     if (this.state.open) {
       return (
         <div>
-          <div className={styles.btnOpen}>
-            <button
-              className={styles.toggleExpandBtn}
-              onClick={this.handleClick}
-            >
-              {'<'}
-            </button>
-          </div>
           <ul className={styles.sidebarOpen}>
             <li>
-              <NavLink exact to="/">
-                Profile List
-              </NavLink>
+              <button className={styles.menuBtn+' '+styles.sidebarBtn} onClick={this.handleClick}>
+                  <img src={menuIcon} className={styles.sidebarIcon}/>
+              </button>
             </li>
             <li>
+              <button className={styles.menuBtn}>
+                <img src={profilesIcon} className={styles.sidebarIcon} />
+              </button>
+              <NavLink exact to="/">Profile List</NavLink>
+            </li>
+            <li>
+            <button className={styles.menuBtn}>
+              <img src={projectsIcon} className={styles.sidebarIcon} />
+            </button>
               <NavLink to="/projects">Projects</NavLink>
             </li>
             <li>
+              <button className={styles.menuBtn}>
+                <img src={analyticsIcon} className={styles.sidebarIcon} />
+              </button>
               <NavLink to="/Analytics">Analytics</NavLink>
             </li>
             <li>
+              <button className={styles.menuBtn}>
+                <img src={calendarIcon} className={styles.sidebarIcon} />
+              </button>
               <NavLink to="/Calendar">Calendar</NavLink>
             </li>
             <li>
+              <button className={styles.menuBtn}>
+                <img src={blackListIcon} className={styles.sidebarIcon} />
+              </button>
               <NavLink to="/BlackList">Black List</NavLink>
             </li>
             <li>
+              <button className={styles.menuBtn}>
+                <img src={userManagmentIcon} className={styles.sidebarIcon} />
+              </button>
               <NavLink to="/UserManagment">User Managment</NavLink>
             </li>
             <span>V.1.0.1</span>
@@ -53,8 +73,26 @@ export class Sidebar extends Component {
     } else {
       return (
         <div>
-          <button className={styles.toggleExpandBtn} onClick={this.handleClick}>
-            >
+          <button className={styles.menuBtn+' '+styles.sidebarBtn} onClick={this.handleClick}>
+            <img src={menuIcon} className={styles.sidebarIcon}/>
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={profilesIcon} className={styles.sidebarIcon} />
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={projectsIcon} className={styles.sidebarIcon} />
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={analyticsIcon} className={styles.sidebarIcon} />
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={calendarIcon} className={styles.sidebarIcon} />
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={blackListIcon} className={styles.sidebarIcon} />
+          </button>
+          <button className={styles.menuBtn}>
+            <img src={userManagmentIcon} className={styles.sidebarIcon} />
           </button>
         </div>
       );
