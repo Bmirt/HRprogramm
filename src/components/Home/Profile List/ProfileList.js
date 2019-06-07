6d1f194e7ed3c41ad2558cb02ebeb01473ebd963
Tamar Menteshashvili <t.menteshashvili@owl.local>
2019-06-07 15:29:24 +0400

    hsj

--- src/components/Home/Profile List/ProfileList.js
+++ src/components/Home/Profile List/ProfileList.js
@@ -4,6 +4,37 @@ import styles from "./ProfileList.module.css";
 
 export default class Home extends Component {
   render() {
-    return <div className={styles.container}>Hello</div>;
+    return (
+      <div className={styles.container}>
+        <table>
+          <tr>
+            <th>Name, Surname</th>
+            <th>Phone</th> 
+            <th>Current Position</th>
+            <th>Profile</th>
+            <th>Portfolio</th> 
+            <th>Technologies</th> 
+            <th>English</th> 
+            <th>Salary Expectation</th> 
+            <th>Source</th> 
+            <th>Status</th> 
+            <th>Projects</th> 
+          </tr>
+          <tr>
+            <td>Jill</td>
+            <td>Smith</td> 
+            <td>50</td>
+            <td>Jill</td>
+            <td>Smith</td> 
+            <td>50</td>
+            <td>Jill</td>
+            <td>Smith</td> 
+            <td>50</td>
+            <td>Jill</td>
+            <td>Smith</td>
+          </tr>
+        </table>
+      </div>
+    )
   }
 }
