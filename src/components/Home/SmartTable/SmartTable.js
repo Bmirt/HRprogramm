829d8fa3f33920a58ca44398c63c642fc26428c7
Tamar Menteshashvili <t.menteshashvili@owl.local>
2019-06-11 12:15:05 +0400

    hay

---
+++ src/components/Home/SmartTable/SmartTable.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react'
+
+class SmartTable extends Component {
+    state = {
+        roughcolumns: [],
+        columnHeaders: this.props.columnHeaders,
+        rows: this.props.rows
+    }
+
+    getElementCoords = (id) => {
+        let coorX0 = document.getElementById(id).offsetLeft;
+        let coorX3 = coorX0 + document.getElementById(id).offsetWidth;
+        let coorX1 = coorX0 + document.getElementById(id).offsetWidth/5;
+        let coorX2 = coorX3 - document.getElementById(id).offsetWidth/5;
+
+        return [coorX0, coorX1, coorX2, coorX3]
+    }
+
+    placeBefore = (movingItem, origin) => {
+        if(movingItem!==origin){
+            let currentArray = this.state.columnHeaders.filter(x=>x!==movingItem);
+            let filteredDown = currentArray.filter(x=>currentArray.indexOf(x)<currentArray.indexOf(origin));
+            let filteredUp = currentArray.filter(x=>currentArray.indexOf(x)>=currentArray.indexOf(origin));
+        
+            this.setState({roughcolumns: [...filteredDown, movingItem, ...filteredUp]});
+        }   
+    }
+    placeAfter = (movingItem, origin) => {
+        if(movingItem!==origin){
+            let currentArray = this.state.columnHeaders.filter(x=>x!==movingItem);
+            let filteredDown = currentArray.filter(x=>currentArray.indexOf(x)<=currentArray.indexOf(origin));
+            let filteredUp = currentArray.filter(x=>currentArray.indexOf(x)>currentArray.indexOf(origin));
+        
+            this.setState({roughcolumns: [...filteredDown, movingItem, ...filteredUp]});
+        } 
+    }
+    handleDragging = (e) => {
+        let coordinates=[]
+        this.state.columnHeaders.map((header) => {
+            coordinates.push([header, ...this.getElementCoords(header)])
+        })
+        coordinates.map((header) => {
+            if(e.clientX===0){}
+            else if(e.clientX < header[4] && e.clientX > header[3]){
+                return this.placeAfter(e.target.id,coordinates[coordinates.indexOf(header)][0])
+            }
+            else if(e.clientX < header[2] && e.clientX > header[1]){
+                return this.placeBefore(e.target.id,coordinates[coordinates.indexOf(header)][0])
+            }
+            else if(e.clientX < header[3] && e.clientX > header[2]){
+                if(coordinates.indexOf(header) < coordinates.indexOf(e.target.id)){
+                    return this.placeAfter(e.target.id,coordinates[coordinates.indexOf(header)][0])
+                }
+                else{
+                    return this.placeBefore(e.target.id,coordinates[coordinates.indexOf(header)][0])
+                }
+            }
+        })
+    }
+    headerRow = () => {
+        let headers = this.state.columnHeaders.map((header) => {
+            return(
+                <th id={header} draggable='true' onDrag={this.handleDragging} onDragEnd={()=>this.setState({columnHeaders: this.state.roughcolumns})}>{header}</th>
+                )
+            })
+        return(
+        <tr>
+            {headers}
+        </tr>
+        )
+    }
+    contentRows =() =>{
+        let rows = this.state.rows.map((row) => {
+            let currentrow = this.state.columnHeaders.map((header)=>{
+                return (
+                    <td>{row[header]}</td>
+                )
+            })
+            return (
+                <tr>{currentrow}</tr>
+            )
+        })
+        return (
+            <tbody>
+                {rows}
+            </tbody>
+        )
+    }    
+    render(){
+        return(
+            <table>
+                <tbody>
+                    {this.headerRow()}
+                </tbody>
+                    {this.contentRows()}
+
+            </table>
+        )
+    }
+    
+    
+}
+
+export default SmartTable
