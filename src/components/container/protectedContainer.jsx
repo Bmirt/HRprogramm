296c7dc3ac530a59dc260c0e4fce03450f2ed022
Iuri Sajaia <i.sajaia@owl.local>
2019-06-13 12:50:50 +0400

    clear app.js

---
+++ src/components/container/protectedContainer.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import Sidebar from "../../containers/SidebarContainer";
+import ProfileList from "../../containers/ProfileListContainer";
+import { Route } from "react-router-dom";
+import Projects from "../Home/Projects/Projects";
+import Analytics from "../Analytics/Analytics";
+import Calendar from "../Calendar/Calendar";
+import BlackList from "../BlackList/BlackList";
+import UserManagement from "../UserManagement/UserManagement";
+
+class ProtectedContainer extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="mainContent">
+          <Sidebar />
+          <Route path="/home/profile_list" exact component={ProfileList} />
+          <Route path="/home/projects" exact component={Projects} />
+          <Route path="/home/analytics" exact component={Analytics} />
+          <Route path="/home/calendar" exact component={Calendar} />
+          <Route path="/home/black_list" exact component={BlackList} />
+          <Route
+            path="/home/user_management"
+            exact
+            component={UserManagement}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProtectedContainer;
