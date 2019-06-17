6c4d1580c4c48074e6d72a26c8d5848e11feae7e
Guram Chechelashvili <gugachechelashvili@yahoo.com>
2019-06-17 12:28:03 +0400

    deleted extra lines

--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -3,33 +3,13 @@ import styles from "./Projects.module.css";
 import { Link } from "react-router-dom";
 
 export default class Home extends Component {
-  state = {
-    technologies: []
-  };
-  generateContent = <div>not found</div>;
-  componentDidMount() {
-    const token = localStorage.getItem("token");
-    fetch("http://laravel.local/api/get-projects", {
-      headers: {
-        "Content-Type": "applcation/json",
-        Authorization: token
-      }
-    })
-      .then(res => res.json())
-      .then(res =>
-        this.setState({
-          technologies: res.projects
-        })
-      );
-  }
 
   render() {
     console.log(this.state);
-    let generateContent = this.state.technologies.map(item => {
+    let generateContent = this.props.projects.map(item => {
       return (
-        <div className={styles.projecthlist}>
+        <div className={styles.projecthlist} key={item.id}>
           <span className={styles.projectitem}>{item.title}</span>
-          {/* <span className={styles.techitem}>{item.profiles.length}</span> */}
           <Link to={`/home/projects/${item.id}`}>
             <button>View</button>{" "}
           </Link>
@@ -42,133 +22,6 @@ export default class Home extends Component {
         <div>
           <div>{generateContent}</div>
         </div>
-        {/* <table >
-        <tbody>
-  <tr>
-    <th>Paylix</th>
-    <th>nOMS</th> 
-    <th>Ag365e</th>
-    <th>newrow</th>
-    <th>Paylix</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-    <th>Project Test</th>
-  </tr>
-  </tbody>
-  <tbody>
-  <tr>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-  </tr>
-  </tbody>
-  <tbody>
-  <tr>
-  <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-  </tr></tbody>
-  <tbody>
-  <tr>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-  </tr>
-  </tbody>
-  <tbody>
-  <tr>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-  </tr>
-  </tbody>
-  <tbody>
-  <tr>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-    <td>Testing</td>
-  </tr>
-  </tbody>
-</table> */}
       </div>
     );
   }
--- src/components/container/protectedContainer.jsx
+++ src/components/container/protectedContainer.jsx
@@ -3,7 +3,8 @@ import Header from "../Header/Header";
 import Sidebar from "../../containers/SidebarContainer";
 // import ProfileList from "../../containers/ProfileListContainer";
 import { Route } from "react-router-dom";
-import Projects from "../Home/Projects/Projects";
+// import Projects from "../Home/Projects/Projects";
+import ProjectsContainer from "../../containers/ProjectsContainer"
 import Analytics from "../Analytics/Analytics";
 import Calendar from "../Calendar/Calendar";
 import BlackList from "../BlackList/BlackList";
@@ -27,7 +28,7 @@ class ProtectedContainer extends Component {
             exact
             component={ProfileListContainer}
           />
-          <Route path="/home/projects" exact component={Projects} />
+          <Route path="/home/projects" exact component={ProjectsContainer} />
           <Route path="/home/analytics" exact component={Analytics} />
           <Route path="/home/calendar" exact component={Calendar} />
           <Route path="/home/black_list" exact component={BlackList} />
